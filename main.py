04460d9d7c505ab9311c88226b93f1aea9b7fd63
David Knott <dknott9782@sohamcollege.org.uk>
2021-01-13 13:00:47 +0000

    stuff

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import turtle
+import random
+for i in range (1000000000000000000000000000000000000) :
+  turtle.speed (0)
+  turtle.right (6700000000000000000000000000000000000000000000000000000000)
+  turtle.forward (i)
+  turtle.left (i)
+  turtle.left (100)
+  turtle.backward (i)
+  turtle.right (i)
 
 
 
